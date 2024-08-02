2d0af655ea688901de84f8019d170315e754926c
AndrewSimWL <49626982+AndrewSimWL@users.noreply.github.com>
2024-08-02 12:46:21 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,8 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+on = st.toggle("Activate feature")
+
+if on:
+    st.write("Feature activated!")
