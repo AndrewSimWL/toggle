4bd53288b9a3536a817a0e6b09cdbeecdf11cfb8
AndrewSimWL <49626982+AndrewSimWL@users.noreply.github.com>
2024-08-02 13:46:04 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,11 @@ st.title("🎈 Saint Chongers Life")
 
 col1, col2, col3 = st.columns(3)
 with col1:
-    toggle1 = st.toggle("Happiness", disabled=toggle6)
+    toggle1 = st.toggle("Happiness")
     toggle2 = st.toggle("Giving")
 with col2:
     toggle3 = st.toggle("Optimism")
     toggle4 = st.toggle("Respect")
 with col3:
     toggle5 = st.toggle("Kindness")
-    toggle6 = st.toggle("Ego")
+    toggle6 = st.toggle("Ego", disabled=toggle1)
