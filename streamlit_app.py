a65bedd4088f977f49d8d88168190a7e29ac07a3
AndrewSimWL <49626982+AndrewSimWL@users.noreply.github.com>
2024-08-02 12:50:09 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,9 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
-on = st.toggle("Activate feature")
-
-if on:
-    st.write("Feature activated!")
+happiness = st.toggle("Happiness")
+optimism = st.toggle("Optimism")
+kindness = st.toggle("Kindness")
+giving = st.toggle("Giving")
+respect = st.toggle("Respect")
+ego = st.toggle("Ego")
