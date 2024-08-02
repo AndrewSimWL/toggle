8bff58a5e38533ac769a8cc41579674e9a495e41
AndrewSimWL <49626982+AndrewSimWL@users.noreply.github.com>
2024-08-02 13:48:56 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,5 @@ with col2:
 with col3:
     toggle5 = st.toggle("Kindness", disabled=disable)
     toggle6 = st.toggle("Ego")
-
-if toggle6:
-    disable=False
+    if toggle6:
+        disable=False
