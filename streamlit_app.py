961c7b8c7e51156aef676a5e0af53a85d9f05daa
AndrewSimWL <49626982+AndrewSimWL@users.noreply.github.com>
2024-08-02 13:12:02 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,19 +3,18 @@ import streamlit as st
 backgroundColor = '#273346'
 
 st.title("🎈 Saint Chongers Life")
-ego_unlock = False
+toggle6_unlock = True
 
 col1, col2, col3 = st.columns(3)
 with col1:
-    happiness = st.toggle("Happiness")
-    giving = st.toggle("Giving")
+    toggle1 = st.toggle("Happiness", disabled=toggle6_unlock)
+    toggle2 = st.toggle("Giving")
 with col2:
-    optimism = st.toggle("Optimism")
-    respect = st.toggle("Respect")
+    toggle3 = st.toggle("Optimism")
+    toggle4 = st.toggle("Respect")
 with col3:
-    kindness = st.toggle("Kindness")
+    toggle5 = st.toggle("Kindness")
+    toggle6 = st.toggle("Ego")
 
-ego_unlock = not(happiness and giving and optimism and respect and kindness)
-
-with col3:
-    ego = st.toggle("Ego", disabled=ego_unlock)
+if toggle6:
+    toggle6_unlock = False
