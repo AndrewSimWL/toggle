abedae18c1ef002bb1e5f5ed05c869ae3e88d105
AndrewSimWL <49626982+AndrewSimWL@users.noreply.github.com>
2024-08-02 12:52:21 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,16 +8,17 @@ st.write(
 )
 
 col1, col2, col3 = st.columns(3)
-
 with col1:
     happiness = st.toggle("Happiness")
-
 with col2:
     optimism = st.toggle("Optimism")
-
 with col3:
     kindness = st.toggle("Kindness")
     
-giving = st.toggle("Giving")
-respect = st.toggle("Respect")
-ego = st.toggle("Ego")
+col4, cold5, col6 = st.column(3)
+with col4:
+    giving = st.toggle("Giving")
+with col5:
+    respect = st.toggle("Respect")
+with col6:
+    ego = st.toggle("Ego")
