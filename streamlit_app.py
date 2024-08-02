ab7d20990a9f03965b0c2a72e339b9eb91a9ce8a
AndrewSimWL <49626982+AndrewSimWL@users.noreply.github.com>
2024-08-02 14:10:37 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,13 +8,13 @@ if st.session_state.get("t6", False):
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    toggle1 = st.toggle("Happiness", disabled=st.session_state.disabled)
-    toggle2 = st.toggle("Giving", disabled=st.session_state.disabled)
+    toggle1 = st.toggle("Happiness", disabled=st.session_state.get("disabled", True))
+    toggle2 = st.toggle("Giving", disabled=st.session_state.get("disabled", True))
 with col2:
-    toggle3 = st.toggle("Optimism", disabled=st.session_state.disabled)
-    toggle4 = st.toggle("Respect", disabled=st.session_state.disabled)
+    toggle3 = st.toggle("Optimism", disabled=st.session_state.get("disabled", True))
+    toggle4 = st.toggle("Respect", disabled=st.session_state.get("disabled", True))
 with col3:
-    toggle5 = st.toggle("Kindness", disabled=st.session_state.disabled)
+    toggle5 = st.toggle("Kindness", disabled=st.session_state.get("disabled", True))
     toggle6 = st.toggle("Ego", key="t6")
 
 if toggle6:
