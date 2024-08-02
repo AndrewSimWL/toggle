cf5d43fa5b1e14581c990cbfbbbbb3cf03c95ef1
AndrewSimWL <49626982+AndrewSimWL@users.noreply.github.com>
2024-08-02 12:53:19 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ st.write(
 col1, col2, col3 = st.columns(3)
 with col1:
     happiness = st.toggle("Happiness")
+    giving = st.toggle("Giving")
 with col2:
     optimism = st.toggle("Optimism")
 with col3:
